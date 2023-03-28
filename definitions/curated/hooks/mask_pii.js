c6cdd0a4d703e9f8ad538ea0a40fbb0070f34050
Wojtek Hejna <whejna@google.com>
2023-03-28 12:37:13 +0000

    deleting policy tags

--- definitions/curated/hooks/mask_pii.js
+++
@@ -1,9 +0,0 @@
-const users = ["fake_user@example.com"]
-
-users.forEach( (user, index) => 
-    operate(`cleanup_pii_user_${index}`).dependencies("cleaned").queries(ctx => `
-    UPDATE \`whejna-modelling-sandbox.dataform_training.cleaned\` 
-    SET author_email = TO_HEX(SHA256(author_email)) 
-    WHERE author_email = '${user}';`)
-    );
-
